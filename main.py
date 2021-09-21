b0889177a5bcbbac92b88af22336ef5186eb6ff8
Andalib Rahman Shagoto <64375438+Shagoto20101195@users.noreply.github.com>
2021-09-21 10:34:41 +0600

    Source

---
+++ main.py
@@ -0,0 +1,63 @@
+import discord
+import requests
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("Bot ready!")
+
+@client.event
+async def on_message(message):
+  # If message from bot itself
+  if(message.author == client.user):
+    return
+
+  text = message.content.lower()
+
+  if(message.content.startswith('>')):
+    if(text[1:] == "hello"):
+      await message.channel.send('Hello {0}'.format(message.author.mention))
+
+    if("happy" in text):
+      await message.channel.send(":smile:")
+
+
+    if("sad" in text):
+      await message.channel.send(":cry:")
+
+    if(text.startswith(">add")):
+      text = text.replace(">add ", "")
+      a, b = map(int, text.split(", "))
+      ans = a+b
+      await message.channel.send("Ans: {0}".format(ans))
+
+    if(text.startswith(">subt")):
+      text = text.replace(">subt ", "")
+      a, b = map(int, text.split(", "))
+      ans = a - b
+      await message.channel.send("Ans: {0}".format(ans))
+
+    if(text.startswith(">mult")):
+      text = text.replace(">mult ", "")
+      a, b = map(int, text.split(", "))
+      ans = a * b
+      await message.channel.send("Ans: {0}".format(ans))
+
+    if(text.startswith(">div")):
+      text = text.replace(">div ", "")
+      a, b = map(int, text.split(", "))
+      ans = a / b
+      await message.channel.send("Ans: {0}".format(ans))
+
+    if(text.startswith(">fdiv")):
+      text = text.replace(">fdiv ", "")
+      a, b = map(int, text.split(", "))
+      ans = a // b
+      await message.channel.send("Ans: {0}".format(ans))
+
+# This token must be hidden
+# client.run(os.getenv('Token')) // If you want it to be private in repl it
+# Store Token = key in .env file (Also don't forget to import os)
+client.run(os.getenv("Token"))
